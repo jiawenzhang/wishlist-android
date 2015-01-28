6a5a48a239c35d2235e1f13f95c2ff089e25a909
Jiawen Zhang <jwzhang@gmail.com>
2015-01-27 23:50:32 -0500

    Fix cast to AnalyticsHelper crash, fix freeze
    Parsing global tracker xml will cause freeze (google bug),
    so configure the tracker programatically

--- wishlist/src/main/java/com/wish/wishlist/AnalyticsHelper.java
+++ wishlist/src/main/java/com/wish/wishlist/AnalyticsHelper.java
@@ -1,8 +1,9 @@
-package com.wish.wishlist.util;
+package com.wish.wishlist;
 
 import android.app.Application;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Logger;
 import com.google.android.gms.analytics.Tracker;
 import com.wish.wishlist.R;
 
@@ -19,7 +20,6 @@ public class AnalyticsHelper extends Application {
 
     public enum TrackerName {
         APP_TRACKER, // Tracker used only in this app.
-        GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg: roll-up tracking.
     }
 
     HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
@@ -33,10 +33,18 @@ public class AnalyticsHelper extends Application {
         if (!mTrackers.containsKey(trackerId)) {
 
             GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-            Tracker t = (trackerId == TrackerName.APP_TRACKER) ? analytics.newTracker(R.xml.app_tracker)
-                    : analytics.newTracker(R.xml.global_tracker);
-            mTrackers.put(trackerId, t);
+            analytics.setLocalDispatchPeriod(30);
 
+            //When true, dryRun flag prevents data from being processed with reports. (for testing)
+            analytics.setDryRun(true);
+
+            analytics.getLogger().setLogLevel(Logger.LogLevel.VERBOSE);
+
+            Tracker t = analytics.newTracker(PROPERTY_ID);
+            t.setSessionTimeout(300);
+            t.enableExceptionReporting(true);
+
+            mTrackers.put(trackerId, t);
         }
         return mTrackers.get(trackerId);
     }
