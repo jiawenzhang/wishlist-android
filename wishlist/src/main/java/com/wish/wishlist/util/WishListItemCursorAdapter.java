a64371db3b7855f54aed59811ae9643687df1735
Jiawen Zhang <jwzhang@gmail.com>
2015-02-08 18:00:13 -0500

    Indent style

--- wishlist/src/main/java/com/wish/wishlist/util/WishListItemCursorAdapter.java
+++ wishlist/src/main/java/com/wish/wishlist/util/WishListItemCursorAdapter.java
@@ -16,49 +16,49 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class WishListItemCursorAdapter extends SimpleCursorAdapter {
-	public WishListItemCursorAdapter(Context context, int layout, Cursor c,
-			String[] from, int[] to) {
-		super(context, layout, c, from, to);
-		setViewBinder(new WishListItemViewBinder());
-	}
-	
-	/***
-	 * WishListItemViewBinder defines how the item's photo and date_time are displayed in 
-	 * the view. 
-	 * 
-	 * It retrieves the image file from the picture_uri saved in database and set the image
-	 * to the view
-	 * 
-	 * It retrieves the date_time from the database and converts it to "July 6, 1983" format
-	 * for display in the view 
-	 */
-	public class WishListItemViewBinder implements SimpleCursorAdapter.ViewBinder {
-		
-		boolean _hasStoreName = false;
-		int _photoWidth;
+    public WishListItemCursorAdapter(Context context, int layout, Cursor c,
+                                     String[] from, int[] to) {
+        super(context, layout, c, from, to);
+        setViewBinder(new WishListItemViewBinder());
+    }
 
-		public WishListItemViewBinder() {
-			//we show 3  or 4 columns of photo in grid view, so photo width should be 1/3 of screen width
+    /***
+     * WishListItemViewBinder defines how the item's photo and date_time are displayed in
+     * the view.
+     *
+     * It retrieves the image file from the picture_uri saved in database and set the image
+     * to the view
+     *
+     * It retrieves the date_time from the database and converts it to "July 6, 1983" format
+     * for display in the view
+     */
+    public class WishListItemViewBinder implements SimpleCursorAdapter.ViewBinder {
+
+        boolean _hasStoreName = false;
+        int _photoWidth;
+
+        public WishListItemViewBinder() {
+            //we show 3  or 4 columns of photo in grid view, so photo width should be 1/3 of screen width
             int columnCount = 3;
             if (Resources.getSystem().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 columnCount = 4;
             }
 
-			int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-			_photoWidth = screenWidth / columnCount;
-		}
+            int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+            _photoWidth = screenWidth / columnCount;
+        }
 
-		@Override
-		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+        @Override
+        public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
             int nImageIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_FULLSIZE_PHOTO_PATH);
 //			int nDateIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_DATE_TIME);
-			int nPriceIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_PRICE);
-			int nStoreNameIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_STORENAME);
-			int nAddIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_ADDRESS);
-			int nCompleteIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_COMPLETE);
-			
-			// set the photo to the image view
-			if (columnIndex == nImageIndex) {
+            int nPriceIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_PRICE);
+            int nStoreNameIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_STORENAME);
+            int nAddIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_ADDRESS);
+            int nCompleteIndex = cursor.getColumnIndexOrThrow(ItemDBManager.KEY_COMPLETE);
+
+            // set the photo to the image view
+            if (columnIndex == nImageIndex) {
                 ImageView imageView = (ImageView) view;
                 String photo_path = cursor.getString(columnIndex);
                 if (photo_path == null) {
@@ -68,7 +68,7 @@ public class WishListItemCursorAdapter extends SimpleCursorAdapter {
                 imageView.setVisibility(View.VISIBLE);
                 Picasso.with(view.getContext()).load(new File(photo_path)).fit().centerCrop().into(imageView);
                 return true;
-			}
+            }
 
 //			// set date and time to the text view in appropriate format
 //			if (columnIndex == nDateIndex) {
@@ -93,97 +93,97 @@ public class WishListItemCursorAdapter extends SimpleCursorAdapter {
 //				viewDate.setText(dateTimeStrNew);
 //				return true;
 //			}
-			
-			else if (columnIndex == nPriceIndex) {
-				TextView viewPrice = (TextView) view;
-				// format the price
-			//	String priceStr = item.getPriceAsString();
-			//	if (priceStr != null) {
-			//		viewPrice.setText("$" + priceStr);
-			//		viewPrice.setVisibility(View.VISIBLE);
-			//	}
-			//	else {
-			//		viewPrice.setVisibility(View.GONE);
-			//	}
 
-				double price = cursor.getDouble(columnIndex);
-				//we use float.min_value to indicate price is not available
-				if (price != Double.MIN_VALUE) {
-					DecimalFormat Dec = new DecimalFormat("0.00");
-					String priceStr = (Dec.format(price));
-					viewPrice.setText(WishItem.priceStringWithCurrency(priceStr, view.getContext()));
-					viewPrice.setVisibility(View.VISIBLE);
-				}
-				else {
-					viewPrice.setVisibility(View.GONE);
-				}
-                return true;
-			}
-			else if (columnIndex == nStoreNameIndex){
-				TextView viewStore = (TextView) view;
-			//	String storeName = item.getStoreName();
-			//	if (!storeName.equals("")) {
-			//		_hasStoreName = true;
-			//		storeName = "At " + storeName;
-			//		viewStore.setText(storeName);
-			//		viewStore.setVisibility(View.VISIBLE);
-			//	}
-			//	else {
-			//		viewStore.setVisibility(View.GONE);
-			//	}
+            else if (columnIndex == nPriceIndex) {
+                TextView viewPrice = (TextView) view;
+                // format the price
+                //	String priceStr = item.getPriceAsString();
+                //	if (priceStr != null) {
+                //		viewPrice.setText("$" + priceStr);
+                //		viewPrice.setVisibility(View.VISIBLE);
+                //	}
+                //	else {
+                //		viewPrice.setVisibility(View.GONE);
+                //	}
 
-				String storeName = cursor.getString(columnIndex);
-				if (!storeName.equals("")) {
-					_hasStoreName = true;
-					storeName = "At " + storeName;
-					viewStore.setText(storeName);
-					viewStore.setVisibility(View.VISIBLE);
-				}
-				else {
-					viewStore.setVisibility(View.GONE);
-				}
+                double price = cursor.getDouble(columnIndex);
+                //we use float.min_value to indicate price is not available
+                if (price != Double.MIN_VALUE) {
+                    DecimalFormat Dec = new DecimalFormat("0.00");
+                    String priceStr = (Dec.format(price));
+                    viewPrice.setText(WishItem.priceStringWithCurrency(priceStr, view.getContext()));
+                    viewPrice.setVisibility(View.VISIBLE);
+                }
+                else {
+                    viewPrice.setVisibility(View.GONE);
+                }
                 return true;
-			}
-			else if (columnIndex == nAddIndex) {
-				TextView viewAddress = (TextView) view;
-			//	String address = item.getAddress();
-			//	if (!address.equals("unknown") && !address.equals("")) {
-			//		if (!_hasStoreName) {
-			//			address = "At " + address;
-			//		}
-			//		viewAddress.setText(address);
-			//		viewAddress.setVisibility(View.VISIBLE);
-			//	}
-			//	else {
-			//		viewAddress.setVisibility(View.GONE);
-			//	}
+            }
+            else if (columnIndex == nStoreNameIndex){
+                TextView viewStore = (TextView) view;
+                //	String storeName = item.getStoreName();
+                //	if (!storeName.equals("")) {
+                //		_hasStoreName = true;
+                //		storeName = "At " + storeName;
+                //		viewStore.setText(storeName);
+                //		viewStore.setVisibility(View.VISIBLE);
+                //	}
+                //	else {
+                //		viewStore.setVisibility(View.GONE);
+                //	}
 
-				String Address = cursor.getString(columnIndex);
-				if (!Address.equals("unknown") && !Address.equals("")) {
-					if (!_hasStoreName) {
-						Address = "At " + Address;
-					}
-					viewAddress.setText(Address);
-					viewAddress.setVisibility(View.VISIBLE);
-				}
-				else {
-					viewAddress.setVisibility(View.GONE);
-				}
+                String storeName = cursor.getString(columnIndex);
+                if (!storeName.equals("")) {
+                    _hasStoreName = true;
+                    storeName = "At " + storeName;
+                    viewStore.setText(storeName);
+                    viewStore.setVisibility(View.VISIBLE);
+                }
+                else {
+                    viewStore.setVisibility(View.GONE);
+                }
                 return true;
-			}
-			else if (columnIndex == nCompleteIndex) {
-				ImageView viewComplete = (ImageView) view;
-				int complete = cursor.getInt(columnIndex);
-				if (complete == 1) {
-					viewComplete.setVisibility(View.VISIBLE);
-				}
-				else {
-					viewComplete.setVisibility(View.INVISIBLE);
-				}
+            }
+            else if (columnIndex == nAddIndex) {
+                TextView viewAddress = (TextView) view;
+                //	String address = item.getAddress();
+                //	if (!address.equals("unknown") && !address.equals("")) {
+                //		if (!_hasStoreName) {
+                //			address = "At " + address;
+                //		}
+                //		viewAddress.setText(address);
+                //		viewAddress.setVisibility(View.VISIBLE);
+                //	}
+                //	else {
+                //		viewAddress.setVisibility(View.GONE);
+                //	}
+
+                String Address = cursor.getString(columnIndex);
+                if (!Address.equals("unknown") && !Address.equals("")) {
+                    if (!_hasStoreName) {
+                        Address = "At " + Address;
+                    }
+                    viewAddress.setText(Address);
+                    viewAddress.setVisibility(View.VISIBLE);
+                }
+                else {
+                    viewAddress.setVisibility(View.GONE);
+                }
                 return true;
-			}
-			return false;
-		}
-	}
+            }
+            else if (columnIndex == nCompleteIndex) {
+                ImageView viewComplete = (ImageView) view;
+                int complete = cursor.getInt(columnIndex);
+                if (complete == 1) {
+                    viewComplete.setVisibility(View.VISIBLE);
+                }
+                else {
+                    viewComplete.setVisibility(View.INVISIBLE);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
 }
 
