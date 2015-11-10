bcd73e49c56317e6ca502e3c32f563f8dc735349
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2015-11-09 22:02:49 -0500

    Send invitation message via email/sms/others

--- wishlist/src/main/java/com/wish/wishlist/fragment/InviteFriendFragmentDialog.java
+++ wishlist/src/main/java/com/wish/wishlist/fragment/InviteFriendFragmentDialog.java
@@ -2,6 +2,8 @@ package com.wish.wishlist.fragment;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
@@ -14,7 +16,10 @@ import com.wish.wishlist.R;
  */
 public class InviteFriendFragmentDialog extends DialogFragment {
 
-    private static final String TAG = "InviteFriendFragmentDialog";
+    private static final String TAG = "InviteFriendFragment";
+    private final static String msgSubject = "Get Beans Wishlist";
+    private final static String msgBody = "Get Beans Wishlist for Android, and let's see each other's wishes\n" +
+            "https://play.google.com/store/apps/details?id=com.wish.wishlist";
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -22,19 +27,43 @@ public class InviteFriendFragmentDialog extends DialogFragment {
         final int FACEBOOK = 0;
         final int EMAIL = 1;
         final int MESSAGE = 2;
-        final String[] mListItems = { "Facebook", "Email", "Message" };
+        final int MORE = 3;
+        final String[] mListItems = { "Facebook", "Email", "Message", "More"};
         dialogBuilder.setItems(mListItems, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int item) {
-                //String selectedText = mListItems[item].toString();
                 switch (item) {
                     case FACEBOOK:
                         Log.d(TAG, "Facebook");
                         break;
                     case EMAIL:
                         Log.d(TAG, "Email");
+                        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                        emailIntent.setData(Uri.parse("mailto:" + ""));
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, msgSubject);
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, msgBody);
+                        try {
+                            startActivity(Intent.createChooser(emailIntent, "Send E-Mail..."));
+                        } catch (android.content.ActivityNotFoundException e) {
+                            Log.e(TAG, e.toString());
+                        }
                         break;
                     case MESSAGE:
                         Log.d(TAG, "Message");
+                        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                        sendIntent.setType("vnd.android-dir/mms-sms");
+                        sendIntent.putExtra("sms_body", msgBody);
+                        startActivity(sendIntent);
+                        break;
+                    case MORE:
+                        final Intent i = new Intent(Intent.ACTION_SEND);
+                        i.setType("text/plain");
+                        i.putExtra(Intent.EXTRA_SUBJECT, msgSubject);
+                        i.putExtra(Intent.EXTRA_TEXT, msgBody);
+                        try {
+                            startActivity(Intent.createChooser(i, ""));
+                        } catch (android.content.ActivityNotFoundException e) {
+                            Log.e(TAG, e.toString());
+                        }
                         break;
                 }
             }
