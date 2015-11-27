25bfe1a7b36e6d883dec401225231ebaa1c13d3f
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2015-11-26 23:49:46 -0500

    Go straight to the app instead of waiting for 2 seconds

--- wishlist/src/main/java/com/wish/wishlist/activity/SplashActivity.java
+++ wishlist/src/main/java/com/wish/wishlist/activity/SplashActivity.java
@@ -9,7 +9,6 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Handler;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
@@ -41,103 +40,99 @@ public class SplashActivity extends Activity {
                 .setAction("Start")
                 .build());
 
-        setContentView(R.layout.splash);
-
-        Handler x = new Handler();
-        x.postDelayed(new splashhandler(), 2000);
         DBAdapter.getInstance().createDB();
+
+        run();
     }
 
-    class splashhandler implements Runnable {
-        public void run() {
-            //show the what's new dialog if necessary
-            SharedPreferences sharedPref = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
-            int currentVersionNumber = 0;
-            int savedVersionNumber = sharedPref.getInt(VERSION_KEY, 0);
-            try {
-                PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
-                currentVersionNumber = pi.versionCode;
-            } catch (Exception e) {}
+    private void run() {
+        //show the what's new dialog if necessary
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        int currentVersionNumber = 0;
+        int savedVersionNumber = sharedPref.getInt(VERSION_KEY, 0);
+        try {
+            PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+            currentVersionNumber = pi.versionCode;
+        } catch (Exception e) {}
 
 
-            if (currentVersionNumber > savedVersionNumber) {
-                if (savedVersionNumber == 23) {
-                    // do db migration, starting from v24, wish latitude and longitude are saved in the item table
-                    // instead of the location table, we should drop store and location table in db schema upgrade
-                    // from v24->v25
-                    // copy the latitude and longitude from location table to item table and save them
-                    ItemDBManager itemDBManager = new ItemDBManager();
-                    ArrayList<Long> ids = itemDBManager.getAllItemIds();
+        if (currentVersionNumber > savedVersionNumber) {
+            if (savedVersionNumber == 23) {
+                // do db migration, starting from v24, wish latitude and longitude are saved in the item table
+                // instead of the location table, we should drop store and location table in db schema upgrade
+                // from v24->v25
+                // copy the latitude and longitude from location table to item table and save them
+                ItemDBManager itemDBManager = new ItemDBManager();
+                ArrayList<Long> ids = itemDBManager.getAllItemIds();
 
-                    for (Long id : ids) {
-                        ItemDBManager.ItemsCursor wishItemCursor = itemDBManager.getItem(id);
-                        long storeID = wishItemCursor.getLong(wishItemCursor.getColumnIndexOrThrow(ItemDBManager.KEY_STORE_ID));
-                        if (storeID == -1) {
-                            continue;
-                        }
-
-                        // Open the Store table in the database
-                        StoreDBManager storeDBManager = new StoreDBManager();
-                        Cursor storeCursor = storeDBManager.getStore(storeID);
-
-                        long locationID = storeCursor.getLong(storeCursor.getColumnIndexOrThrow(StoreDBManager.KEY_LOCATION_ID));
-
-                        LocationDBManager locationDBManager = new LocationDBManager();
-                        double latitude = locationDBManager.getLatitude(locationID);
-                        double longitude =  locationDBManager.getLongitude(locationID);
-
-                        WishItem item = WishItemManager.getInstance().getItemById(id);
-                        item.setLatitude(latitude);
-                        item.setLongitude(longitude);
-                        item.saveToLocal();
+                for (Long id : ids) {
+                    ItemDBManager.ItemsCursor wishItemCursor = itemDBManager.getItem(id);
+                    long storeID = wishItemCursor.getLong(wishItemCursor.getColumnIndexOrThrow(ItemDBManager.KEY_STORE_ID));
+                    if (storeID == -1) {
+                        continue;
                     }
 
-                    // generate thumbnail image for all wishes. starting from v24, we save a scaled down image as thumbnail
-                    // for each wish. we display the thumbnail image in list/grid view, we also sync the thumbnail image using Parse
-                    for (Long id : ids) {
-                        WishItem item = WishItemManager.getInstance().getItemById(id);
-                        String fullsizeImagePath = item.getFullsizePicPath();
-                        if (fullsizeImagePath != null) {
-                            ImageManager.saveBitmapToThumb(fullsizeImagePath);
-                        }
-                    }
+                    // Open the Store table in the database
+                    StoreDBManager storeDBManager = new StoreDBManager();
+                    Cursor storeCursor = storeDBManager.getStore(storeID);
+
+                    long locationID = storeCursor.getLong(storeCursor.getColumnIndexOrThrow(StoreDBManager.KEY_LOCATION_ID));
+
+                    LocationDBManager locationDBManager = new LocationDBManager();
+                    double latitude = locationDBManager.getLatitude(locationID);
+                    double longitude =  locationDBManager.getLongitude(locationID);
+
+                    WishItem item = WishItemManager.getInstance().getItemById(id);
+                    item.setLatitude(latitude);
+                    item.setLongitude(longitude);
+                    item.saveToLocal();
                 }
 
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putInt(VERSION_KEY, currentVersionNumber);
-                editor.commit();
-                //startActivity(new Intent(getApplication(), NewFeatureFragmentActivity.class));
-                //startActivity(new Intent(getApplication(), MyWishActivity.class));
-            } else {
-                //startActivity(new Intent(getApplication(), MyWishActivity.class));
+                // generate thumbnail image for all wishes. starting from v24, we save a scaled down image as thumbnail
+                // for each wish. we display the thumbnail image in list/grid view, we also sync the thumbnail image using Parse
+                for (Long id : ids) {
+                    WishItem item = WishItemManager.getInstance().getItemById(id);
+                    String fullsizeImagePath = item.getFullsizePicPath();
+                    if (fullsizeImagePath != null) {
+                        ImageManager.saveBitmapToThumb(fullsizeImagePath);
+                    }
+                }
             }
 
-            Intent intent = new Intent(getApplication(), UserLoginActivity.class);
-            intent.putExtra(UserLoginActivity.FROM_SPLASH, true);
-            startActivity(intent);
-
-            SplashActivity.this.finish();
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt(VERSION_KEY, currentVersionNumber);
+            editor.commit();
+            //startActivity(new Intent(getApplication(), NewFeatureFragmentActivity.class));
+            //startActivity(new Intent(getApplication(), MyWishActivity.class));
+        } else {
+            //startActivity(new Intent(getApplication(), MyWishActivity.class));
         }
 
-        private void showWhatsNewDialog() {
-            AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this, R.style.AppCompatAlertDialogStyle);
-            builder.setTitle("What's new");
-            builder.setMessage("Version 1.1.1\n\n" +
-                    "Completely renovated grid view, showing wishes in multi-column staggered fashion.\n\n" +
-                    "Improved list view.\n\n" +
-                    "Loads wish images more efficiently.\n");
+        Intent intent = new Intent(getApplication(), UserLoginActivity.class);
+        intent.putExtra(UserLoginActivity.FROM_SPLASH, true);
+        startActivity(intent);
 
-            builder.setCancelable(false);
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    dialog.dismiss();
-                    startActivity(new Intent(getApplication(), MyWishActivity.class));
-                    SplashActivity.this.finish();
-                }
-            });
-            AlertDialog dialog = builder.create();
-            //dialog.setOnShowListener(new DialogOnShowListener(SplashActivity.this));
-            dialog.show();
-        }
+        SplashActivity.this.finish();
+    }
+
+    private void showWhatsNewDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this, R.style.AppCompatAlertDialogStyle);
+        builder.setTitle("What's new");
+        builder.setMessage("Version 1.1.1\n\n" +
+                "Completely renovated grid view, showing wishes in multi-column staggered fashion.\n\n" +
+                "Improved list view.\n\n" +
+                "Loads wish images more efficiently.\n");
+
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+                startActivity(new Intent(getApplication(), MyWishActivity.class));
+                SplashActivity.this.finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        //dialog.setOnShowListener(new DialogOnShowListener(SplashActivity.this));
+        dialog.show();
     }
 }
