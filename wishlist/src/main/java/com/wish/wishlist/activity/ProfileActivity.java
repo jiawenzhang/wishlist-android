0318d15c4f58c22a774b658a1d49b180fee1fea3
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2015-12-05 16:36:35 -0500

    Re-launch the app when user logout

--- wishlist/src/main/java/com/wish/wishlist/activity/ProfileActivity.java
+++ wishlist/src/main/java/com/wish/wishlist/activity/ProfileActivity.java
@@ -187,8 +187,13 @@ public class ProfileActivity extends ActivityBase implements
                 return true;
             case R.id.menu_profile_logout:
                 mUser.logOut();
-                finish();
-                return true;
+
+                // re-launch the app
+                Intent i = getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage(getBaseContext().getPackageName() );
+
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
             default:
                 return super.onOptionsItemSelected(item);
         }
