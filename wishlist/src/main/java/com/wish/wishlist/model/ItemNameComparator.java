326c6e3bf792a484fa1a778deb3fd4ceab153c23
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2016-01-06 21:39:36 -0500

    Case insensitive when sorting by name

--- wishlist/src/main/java/com/wish/wishlist/model/ItemNameComparator.java
+++ wishlist/src/main/java/com/wish/wishlist/model/ItemNameComparator.java
@@ -8,6 +8,6 @@ import java.util.Comparator;
 public class ItemNameComparator implements Comparator<WishItem> {
     @Override
     public int compare(WishItem o1, WishItem o2) {
-        return o1.getName().compareTo(o2.getName());
+        return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
     }
 }
