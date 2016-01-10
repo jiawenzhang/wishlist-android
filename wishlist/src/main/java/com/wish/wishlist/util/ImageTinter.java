f71fb43970ee93b4f8f8e2172775f55f383066c1
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2016-01-09 19:27:54 -0500

    Update add friend/friends icons

---
+++ wishlist/src/main/java/com/wish/wishlist/util/ImageTinter.java
@@ -0,0 +1,33 @@
+package com.wish.wishlist.util;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+
+import com.wish.wishlist.WishlistApplication;
+
+/**
+ * Created by jiawen on 2016-01-09.
+ */
+public class ImageTinter {
+    private static ImageTinter ourInstance = new ImageTinter();
+
+    public static ImageTinter getInstance() {
+        return ourInstance;
+    }
+
+    private ImageTinter() {
+    }
+
+    Drawable tint(int resId) {
+        Drawable drawable = ContextCompat.getDrawable(WishlistApplication.getAppContext(), resId);
+        drawable = DrawableCompat.wrap(drawable);
+        DrawableCompat.setTint(drawable, Color.WHITE);
+
+        // SRC_IN replace the original color instead of mixing with it
+        DrawableCompat.setTintMode(drawable, PorterDuff.Mode.SRC_IN);
+        return drawable;
+    }
+}
