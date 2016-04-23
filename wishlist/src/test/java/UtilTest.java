13f9419554fd442f6553c4a499bcd7ceb09dd249
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2016-04-22 23:32:10 -0400

    Initial util unit test

---
+++ wishlist/src/test/java/UtilTest.java
@@ -0,0 +1,35 @@
+import com.wish.wishlist.util.DoubleUtil;
+import com.wish.wishlist.util.StringUtil;
+
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UtilTest {
+
+    @Test
+    public void DoubleUtil() {
+        //assertEquals("Conversion from celsius to fahrenheit failed", 1, 1, 0.001);
+        assertTrue(DoubleUtil.compare(0.1, 0.1));
+        assertTrue(DoubleUtil.compare(null, null));
+        assertTrue(!DoubleUtil.compare(0.2, null));
+        assertTrue(!DoubleUtil.compare(null, 0.2));
+        assertTrue(!DoubleUtil.compare(0.1, 0.2));
+    }
+
+    @Test
+    public void StringUtil() {
+        assertTrue(StringUtil.compare("test", "test"));
+        assertTrue(StringUtil.compare(null, null));
+        assertTrue(StringUtil.compare("", ""));
+        assertTrue(!StringUtil.compare(null, "test"));
+        assertTrue(!StringUtil.compare("test", null));
+        assertTrue(!StringUtil.compare("test1", "test2"));
+    }
+}
