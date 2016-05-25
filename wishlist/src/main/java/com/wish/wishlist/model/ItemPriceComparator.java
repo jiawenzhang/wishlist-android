d984b2be49aa8d35664e63c312a22fffc6cb74f2
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2016-05-25 00:04:04 -0400

    Fix null crash when sorting by price

--- wishlist/src/main/java/com/wish/wishlist/model/ItemPriceComparator.java
+++ wishlist/src/main/java/com/wish/wishlist/model/ItemPriceComparator.java
@@ -8,6 +8,23 @@ import java.util.Comparator;
 public class ItemPriceComparator implements Comparator<WishItem> {
     @Override
     public int compare(WishItem o1, WishItem o2) {
+        Double d1 = o1.getPrice();
+        Double d2 = o2.getPrice();
+
+        if (d1 == null && d2 == null) {
+            return 0;
+        }
+
+        if (d1 == null) {
+            // d1 < d2
+            return -1;
+        }
+
+        if (d2 == null) {
+            // d1 > d2
+            return 1;
+        }
+
         return Double.compare(o1.getPrice(), o2.getPrice());
     }
 }
