224706205bbc67e0cbd1030e4e16cb5fe594580c
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2016-05-29 18:01:53 -0400

    Fix crash when getting owner id while account is not enabled
    - release v36 (1.2.10)

--- wishlist/src/main/java/com/wish/wishlist/util/Owner.java
+++ wishlist/src/main/java/com/wish/wishlist/util/Owner.java
@@ -1,15 +1,22 @@
 package com.wish.wishlist.util;
 
 import com.parse.ParseUser;
+import com.wish.wishlist.R;
+import com.wish.wishlist.WishlistApplication;
 
 /**
  * Created by jiawen on 2016-05-23.
  */
 public class Owner {
     public static String id() {
+        if (!WishlistApplication.getAppContext().getResources().getBoolean(R.bool.enable_account)) {
+            return null;
+        }
+
         if (ParseUser.getCurrentUser() == null) {
             return null;
         }
+
         return ParseUser.getCurrentUser().getObjectId();
     }
 }
