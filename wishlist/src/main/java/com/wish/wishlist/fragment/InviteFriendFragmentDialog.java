65c3e829988e68b5cf092822bd459cf997b97a98
Jiawen Zhang <jiawenzhang.jwz@gmail.com>
2016-07-11 21:43:00 -0400

    Add my own account name in invite message

--- wishlist/src/main/java/com/wish/wishlist/fragment/InviteFriendFragmentDialog.java
+++ wishlist/src/main/java/com/wish/wishlist/fragment/InviteFriendFragmentDialog.java
@@ -9,6 +9,7 @@ import android.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
+import com.parse.ParseUser;
 import com.wish.wishlist.R;
 
 /**
@@ -18,8 +19,7 @@ public class InviteFriendFragmentDialog extends DialogFragment {
 
     private static final String TAG = "InviteFriendFragment";
     private final static String msgSubject = "Get Beans Wishlist";
-    private final static String msgBody = "Get Beans Wishlist for Android, and let's see each other's wishes\n" +
-            "https://play.google.com/store/apps/details?id=com.wish.wishlist";
+    private final static String appUrl = "https://play.google.com/store/apps/details?id=com.wish.wishlist";
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -30,6 +30,15 @@ public class InviteFriendFragmentDialog extends DialogFragment {
         final int MORE = MESSAGE + 1;
         //final String[] mListItems = { "Facebook", "Email", "Message", "More"};
         final String[] mListItems = { "Email", "Message", "More"};
+
+        String body = "Get Beans Wishlist for Android, and let's see each other's wishes.\n\n" + appUrl;
+        ParseUser user = ParseUser.getCurrentUser();
+        if (user != null && user.getString("name") != null) {
+            body = "Hi, this is " + user.getString("name") + " on Beans Wishlist. Find me as \"" +
+                    user.getString("name") + "\"" +
+                    " in the app and add me as a friend. We can start seeing each other's wishes!\n\n" + appUrl;
+        }
+        final String msgBody = body;
         dialogBuilder.setItems(mListItems, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int item) {
                 switch (item) {
